b2df135b65578f9fe455768333bc903649a8b98f
Justin Holzmann <jholzmann@8thlight.com>
2016-09-09 18:32:31 -0500

    Introduce new statement to pass test 2
    Test 1 is now failing. We will now introduce a new transformation.

--- src/main/java/transformer/PrimeFactors.java
+++ src/main/java/transformer/PrimeFactors.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class PrimeFactors {
    public static List<Integer> generate(int i) {
        ArrayList<Integer> factors = new ArrayList<>();
+       factors.add(2);
        return factors;
     }
 }
