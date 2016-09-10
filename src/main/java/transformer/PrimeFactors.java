1c6888bfeb2b04af0f439441943237f29651ec2f
Justin Holzmann <jholzmann@8thlight.com>
2016-09-09 20:46:27 -0500

    Pass 4 with recursion.

--- src/main/java/transformer/PrimeFactors.java
+++ src/main/java/transformer/PrimeFactors.java
@@ -6,8 +6,19 @@ import java.util.List;
 public class PrimeFactors {
    public static List<Integer> generate(int i) {
        List<Integer> primeFactors = new ArrayList<>();
-       if (i > 1)
-           primeFactors.add(i);
-       return primeFactors;
+
+       return generateIter(i, primeFactors);
+    }
+
+    private static List<Integer> generateIter(int i, List<Integer> primeFactors) {
+        if (i == 1) {
+            return primeFactors;
+        } else if (i % 2 == 0) {
+            primeFactors.add(2);
+            return generateIter(i / 2, primeFactors);
+        } else {
+            primeFactors.add(i);
+            return primeFactors;
+        }
     }
 }
