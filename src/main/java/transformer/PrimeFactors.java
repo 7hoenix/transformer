60cf103277384bd8c952b5691549bccd2aa181fd
Justin Holzmann <jholzmann@8thlight.com>
2016-09-09 20:17:43 -0500

    Experiment with many examples.

--- src/main/java/transformer/PrimeFactors.java
+++ src/main/java/transformer/PrimeFactors.java
@@ -6,12 +6,29 @@ import java.util.List;
 public class PrimeFactors {
    public static List<Integer> generate(int i) {
        List<Integer> primeFactors = new ArrayList<>();
+       if (i % 2 == 0) {
+           primeFactors.add(2);
+           i = i / 2;
+       }
+
        if (i == 2)
            primeFactors.add(2);
        else if (i == 3)
            primeFactors.add(3);
        else if (i == 5)
            primeFactors.add(5);
+       else if (i == 7)
+           primeFactors.add(7);
+       else if (i == 11)
+           primeFactors.add(11);
+       else if (i == 13)
+           primeFactors.add(13);
+       else if (i == 17)
+           primeFactors.add(17);
+       else if (i == 19)
+           primeFactors.add(19);
+       else if (i == 23)
+           primeFactors.add(23);
 
        if (i == 4) {
            primeFactors.add(2);
@@ -19,7 +36,63 @@ public class PrimeFactors {
        } else if (i == 6) {
            primeFactors.add(2);
            primeFactors.add(3);
+       } else if (i == 8) {
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(2);
+       } else if (i == 9) {
+           primeFactors.add(3);
+           primeFactors.add(3);
+       } else if (i == 10) {
+           primeFactors.add(2);
+           primeFactors.add(5);
+       } else if (i == 12) {
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(3);
+       } else if (i == 14) {
+           primeFactors.add(2);
+           primeFactors.add(7);
+       } else if (i == 15) {
+           primeFactors.add(3);
+           primeFactors.add(5);
+       } else if (i == 16) {
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(2);
+       } else if (i == 18) {
+           primeFactors.add(2);
+           primeFactors.add(3);
+           primeFactors.add(3);
+       } else if (i == 20) {
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(5);
+       } else if (i == 21) {
+           primeFactors.add(3);
+           primeFactors.add(7);
+       } else if (i == 22) {
+           primeFactors.add(2);
+           primeFactors.add(11);
+       } else if (i == 24) {
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(2);
+           primeFactors.add(3);
+       } else if (i == 25) {
+           primeFactors.add(5);
+           primeFactors.add(5);
+       } else if (i == 26) {
+           primeFactors.add(2);
+           primeFactors.add(13);
+       } else if (i == 27) {
+           primeFactors.add(3);
+           primeFactors.add(3);
+           primeFactors.add(3);
        }
+
+
        return primeFactors;
     }
 }
--- src/test/java/transformer/PrimeFactorsTest.java
+++ src/test/java/transformer/PrimeFactorsTest.java
@@ -38,4 +38,29 @@ public class PrimeFactorsTest {
     public void factors_of_six() throws Exception {
         assertEquals(Arrays.asList(2, 3), PrimeFactors.generate(6));
     }
+
+    @Test
+    public void factors_of_a_lot_of_things() throws Exception {
+        assertEquals(Arrays.asList(7), PrimeFactors.generate(7));
+        assertEquals(Arrays.asList(2, 2, 2), PrimeFactors.generate(8));
+        assertEquals(Arrays.asList(3, 3), PrimeFactors.generate(9));
+        assertEquals(Arrays.asList(2, 5), PrimeFactors.generate(10));
+        assertEquals(Arrays.asList(11), PrimeFactors.generate(11));
+        assertEquals(Arrays.asList(2, 2, 3), PrimeFactors.generate(12));
+        assertEquals(Arrays.asList(13), PrimeFactors.generate(13));
+        assertEquals(Arrays.asList(2, 7), PrimeFactors.generate(14));
+        assertEquals(Arrays.asList(3, 5), PrimeFactors.generate(15));
+        assertEquals(Arrays.asList(2, 2, 2, 2), PrimeFactors.generate(16));
+        assertEquals(Arrays.asList(17), PrimeFactors.generate(17));
+        assertEquals(Arrays.asList(2, 3, 3), PrimeFactors.generate(18));
+        assertEquals(Arrays.asList(19), PrimeFactors.generate(19));
+        assertEquals(Arrays.asList(2, 2, 5), PrimeFactors.generate(20));
+        assertEquals(Arrays.asList(3, 7), PrimeFactors.generate(21));
+        assertEquals(Arrays.asList(2, 11), PrimeFactors.generate(22));
+        assertEquals(Arrays.asList(23), PrimeFactors.generate(23));
+        assertEquals(Arrays.asList(2, 2, 2, 3), PrimeFactors.generate(24));
+        assertEquals(Arrays.asList(5, 5), PrimeFactors.generate(25));
+        assertEquals(Arrays.asList(2, 13), PrimeFactors.generate(26));
+        assertEquals(Arrays.asList(3, 3, 3), PrimeFactors.generate(27));
+    }
 }
